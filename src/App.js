aeaf7c95297877e608eeae5e4cda52b105e7f50b
Jane <janechodance@gmail.com>
2022-03-25 10:56:34 -0400

    Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import Accordion from "./components/Accordion";
 import Dropdown from "./components/Dropdown";
+import Route from "./components/Route";
 import Search from "./components/Search";
 import Translate from "./components/Translate";
 
@@ -39,14 +40,22 @@ function App(){
     
     return(
         <div>
-            
-            {/* <Accordion items={items}/> */}
-            {/* <Search /> */}
-             {/* <Dropdown
+            <Route path="/">
+                <Accordion items={items}/>
+            </Route>
+            <Route path="/list">
+                <Search/>
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown
              selected={selected}
              onSelectedChange={setSelected}
-             options={options} />  */}
-             <Translate/>
+             options={options} /> 
+            </Route>
+            <Route path="/translate">
+                <Translate/>
+            </Route>
+            
         </div>
     )
 }
